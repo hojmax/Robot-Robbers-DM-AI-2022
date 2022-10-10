421775926e934a0e83a1670fb02857c8c862b42e
hojmax <axelhojmark@gmail.com>
2022-10-10 19:22:49 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ from predict import get_action
 if __name__ == '__main__':
     env = RobotRobbersEnv()
     state = env.reset(42)
-    n_steps = 3000
+    n_steps = 10000
     for i in range(n_steps):
         action = get_action(state)
         state, reward, done, info = env.step(action)
