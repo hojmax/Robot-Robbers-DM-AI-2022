c4733f1ca62602929b4db9fb7ebb0793397c9c6e
hojmax <axelhojmark@gmail.com>
2022-10-10 18:41:44 +0200

    Working queue (without values)

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from environment import RobotRobbersEnv
 from predict import get_action
 
 if __name__ == '__main__':
-    import pygame
+
     env = RobotRobbersEnv()
+
+    state = env.reset()
+    for step in range(1):
+        action = get_action(state)
+        print('Action:', action)
+        state, reward, done, info = env.step(action)
 
     # # --- Static testing
     # for ep in range(100):
@@ -15,11 +21,11 @@ if __name__ == '__main__':
     #         print(f'Episode: {ep}, Step: {step}, Total Reward: {info["total_reward"]}')
 
     # --- Dynamic testing
-    state = env.reset()
-    clock = pygame.time.Clock()
-    while True:
-        clock.tick(24)
-        action = get_action(state)
-        # print('Action:', action)
-        state, reward, done, info = env.step(action)
-        env.render()
+    # state = env.reset()
+    # clock = pygame.time.Clock()
+    # while True:
+    #     clock.tick(24)
+    #     action = get_action(state)
+    #     # print('Action:', action)
+    #     state, reward, done, info = env.step(action)
+    #     env.render()
